a326a198ec57c66b25d530f66fce34c396d1cc9f
anthony-ly <director2911@gmail.com>
2022-04-02 23:26:13 +1100

    Moved code from main() method to MyClient constructor.

--- stage1/src/MyClient.java
+++ stage1/src/MyClient.java
@@ -29,9 +29,6 @@ public class MyClient {
 
     // Find the first server of the largest type
     public static Server findLargestServer(ArrayList<Server> serverList) {
-        // null checks??
-        // check for null, if null, return null, then once you return to main program
-        // if the return value is null, exit the simulation
         Server largestServer = serverList.get(0);
         int largestCore = largestServer.core;
 
@@ -47,7 +44,6 @@ public class MyClient {
 
     // Find the number of servers of that type
     public static int largestServerNumber(ArrayList<Server> serverList, Server largestServer) {
-        // null check?? maybe not needed because if this gets run then both lserver and slist will != null
         int counter = 0;
         for (Server s : serverList) {
             if (s.serverType.equals(largestServer.serverType)) {
@@ -60,6 +56,7 @@ public class MyClient {
         return counter;
     }
 
+    // Creates socket connection with ds-server and opens input and output streams
     public static boolean openConnection(String hostID, int serverPort) {
         try {
             s = new Socket(hostID, serverPort);
@@ -76,6 +73,7 @@ public class MyClient {
         return false;
     }
 
+    // Closes socket connection with ds-server and closes input and output streams
     public static void closeConnection() {
         try {
             input.close();
@@ -87,136 +85,142 @@ public class MyClient {
         }
     }
 
+        // Constructor
+        public MyClient(String hostID, int serverPort) {
+            if (!openConnection(hostID, serverPort)) {
+                return;
+            }
+    
+            try {
+                // HANDSHAKE
+                writeMessage("HELO");
+    
+                serverMessage = input.readLine();
+                System.out.println("SERVER: " + serverMessage); // OK
+    
+                writeMessage("AUTH " + username);
+    
+                serverMessage = input.readLine();
+                System.out.println("SERVER: " + serverMessage); // OK
+    
+                writeMessage("REDY");
+    
+                serverMessage = input.readLine();
+                System.out.println("SERVER: " + serverMessage); // JOBN
+    
+                String jobn = serverMessage; // store the first job
+    
+                writeMessage("GETS All");
+    
+                serverMessage = input.readLine(); // DATA
+                System.out.println("SERVER: " + serverMessage); // DATA
+    
+                // Split the DATA response
+                int serverNums = 0;
+                if (serverMessage.contains("DATA")) {
+                    String[] serverData = serverMessage.split(" ");
+    
+                    String nRecs = serverData[1]; // store the number of servers
+    
+                    if (!nRecs.equals(".")) {
+                        serverNums = Integer.parseInt(nRecs); // store number of servers as int
+                    } else {
+                        serverNums = -1;
+                    }
+                }
+    
+                // TODO: ERROR HANDLING FOR IF NO RECORDS
+                writeMessage("OK");
+                // serverMessage = input.readLine();
+    
+                ArrayList<Server> serverList = new ArrayList<Server>();
+    
+                // find the largest server type
+                // loop iterates through all the servers
+                // if the current server has more cores than the current value of largestCores
+                // update the LargestServer object to now refer to the new highest server
+                for (int i = 0; i < serverNums; i++) { 
+                    // get the next server info
+                    serverMessage = input.readLine();
+    
+                    // add the server to the array list
+                    serverList.add(new Server(serverMessage));
+                    
+                    // System.out.println("SERVER: " + serverMessage); // Server info
+                }
+    
+                // now at this point, we have an arraylist that contains all the available servers
+                // lets pass this to a helper function which will determine which server is of the largest type
+                Server largestServer = findLargestServer(serverList);
+    
+                // now that we know the largest server, we need to find out how many servers of that type exist
+                int serverLargestMax = largestServerNumber(serverList, largestServer);
+    
+                writeMessage("OK");
+    
+                serverMessage = input.readLine(); // .
+                System.out.println("SERVER: " + serverMessage); // .
+    
+                int LRRServerIncrement = 0;
+                
+    
+                while (true) {
+    
+                    if (serverMessage.contains("JCPL")) {
+                        writeMessage("REDY");
+    
+                        serverMessage = input.readLine();
+                        jobn = serverMessage;
+                        System.out.println("SERVER: " + serverMessage); // JOBN
+    
+                        continue;
+                    }
+    
+                    if (serverMessage.contains("NONE")) {
+                        break;
+                    }
+    
+                    // Check if the loop has exceeded the maximum number of available servers
+                    // of largest type
+                    // If so, reset to 0
+                    if (LRRServerIncrement > serverLargestMax) {
+                        LRRServerIncrement = 0;
+                    }
+    
+                    // Schedule jobs
+                    Job job = new Job(jobn);
+                    writeMessage("SCHD " + job.jobID + " " + largestServer.serverType + " " + LRRServerIncrement);
+                    
+                    serverMessage = input.readLine();
+                    System.out.println("SERVER: " + serverMessage); // OK
+    
+                    LRRServerIncrement += 1;
+    
+                    writeMessage("REDY");
+    
+                    serverMessage = input.readLine();
+                    System.out.println("SERVER: " + serverMessage); // JOBN, JCPL etc.
+    
+                    jobn = serverMessage;
+                }
+    
+                writeMessage("QUIT");
+    
+                serverMessage = input.readLine();
+                System.out.println("SERVER: " + serverMessage);
+    
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+    
+            closeConnection();
+        }
+
     public static void main(String args[]) {
         // arguments supply hostname of destination
         serverPort = 50000;
 
-        if (!openConnection(args[0], serverPort)) {
-            return;
-        }
+        MyClient client = new MyClient(args[0], serverPort);
 
-        try {
-            // HANDSHAKE
-            writeMessage("HELO");
-
-            serverMessage = input.readLine();
-            System.out.println("SERVER: " + serverMessage); // OK
-
-            writeMessage("AUTH " + username);
-
-            serverMessage = input.readLine();
-            System.out.println("SERVER: " + serverMessage); // OK
-
-            writeMessage("REDY");
-
-            serverMessage = input.readLine();
-            System.out.println("SERVER: " + serverMessage); // JOBN
-
-            String jobn = serverMessage; // store the first job
-
-            writeMessage("GETS All");
-
-            serverMessage = input.readLine(); // DATA
-            System.out.println("SERVER: " + serverMessage); // DATA
-
-            // Split the DATA response
-            int serverNums = 0;
-            if (serverMessage.contains("DATA")) {
-                String[] serverData = serverMessage.split(" ");
-
-                String nRecs = serverData[1]; // store the number of servers
-
-                if (!nRecs.equals(".")) {
-                    serverNums = Integer.parseInt(nRecs); // store number of servers as int
-                } else {
-                    serverNums = -1;
-                }
-            }
-
-            // TODO: ERROR HANDLING FOR IF NO RECORDS
-            writeMessage("OK");
-            // serverMessage = input.readLine();
-
-            ArrayList<Server> serverList = new ArrayList<Server>();
-
-            // find the largest server type
-            // loop iterates through all the servers
-            // if the current server has more cores than the current value of largestCores
-            // update the LargestServer object to now refer to the new highest server
-            for (int i = 0; i < serverNums; i++) { 
-                // get the next server info
-                serverMessage = input.readLine();
-
-                // add the server to the array list
-                serverList.add(new Server(serverMessage));
-                
-                // System.out.println("SERVER: " + serverMessage); // Server info
-            }
-
-            // now at this point, we have an arraylist that contains all the available servers
-            // lets pass this to a helper function which will determine which server is of the largest type
-            Server largestServer = findLargestServer(serverList);
-
-            // now that we know the largest server, we need to find out how many servers of that type exist
-            int serverLargestMax = largestServerNumber(serverList, largestServer);
-
-            writeMessage("OK");
-
-            serverMessage = input.readLine(); // .
-            System.out.println("SERVER: " + serverMessage); // .
-
-            int LRRServerIncrement = 0;
-            
-
-            while (true) {
-
-                if (serverMessage.contains("JCPL")) {
-                    writeMessage("REDY");
-
-                    serverMessage = input.readLine();
-                    jobn = serverMessage;
-                    System.out.println("SERVER: " + serverMessage); // JOBN
-
-                    continue;
-                }
-
-                if (serverMessage.contains("NONE")) {
-                    break;
-                }
-
-                // Check if the loop has exceeded the maximum number of available servers
-                // of largest type
-                // If so, reset to 0
-                if (LRRServerIncrement > serverLargestMax) {
-                    LRRServerIncrement = 0;
-                }
-
-                // Schedule jobs
-                Job job = new Job(jobn);
-                writeMessage("SCHD " + job.jobID + " " + largestServer.serverType + " " + LRRServerIncrement);
-                
-                serverMessage = input.readLine();
-                System.out.println("SERVER: " + serverMessage); // OK
-
-                LRRServerIncrement += 1;
-
-                writeMessage("REDY");
-
-                serverMessage = input.readLine();
-                System.out.println("SERVER: " + serverMessage); // JOBN, JCPL etc.
-
-                jobn = serverMessage;
-            }
-
-            writeMessage("QUIT");
-
-            serverMessage = input.readLine();
-            System.out.println("SERVER: " + serverMessage);
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        closeConnection();
     }
 }
