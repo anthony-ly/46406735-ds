c2a7402be3656accb3a07e950c74b03113ed2b76
anthony-ly <director2911@gmail.com>
2022-05-19 23:38:59 +1000

    Modified MyClient.java to use MyAlgorithm.

--- stage2/src/MyClient.java
+++ stage2/src/MyClient.java
@@ -64,6 +64,8 @@ public class MyClient {
         }
         if (schedAlgo.equals("lrr")) {
             schedulingAlgorithm = new LRR(input, output);
+        } else if (schedAlgo.equals("s")) {
+            schedulingAlgorithm = new MyAlgorithm(input, output);
         }
 
         try {
