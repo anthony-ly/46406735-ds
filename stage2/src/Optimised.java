5e02d8c53fa9238e1b134a836e88e35560717d0f
anthony-ly <director2911@gmail.com>
2022-05-24 19:32:07 +1000

    Added starting code for Optimised.java.

---
+++ stage2/src/Optimised.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Optimised extends Algorithm {
+
+    public Optimised(BufferedReader in, DataOutputStream out) {
+        super(in, out);
+    }
+
+    @Override
+    public void run() throws IOException {
+        
+    }
+
+}
